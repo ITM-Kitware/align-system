2233b6434381a337c7db958c5b2ee348150c8b46
David Joy <10147749+dmjoy@users.noreply.github.com>
2025-04-16 16:26:44 -0400

    WIP updates for prompt-based aligned pipeline ADM

--- align_system/algorithms/icl_adm_component.py
+++ align_system/algorithms/icl_adm_component.py
@@ -1,7 +1,13 @@
-from align_system.utils import call_with_coerced_args
+from functools import lru_cache
+
+from swagger_client.models import KDMAValue
+
+from align_system.utils import logging, call_with_coerced_args
 from align_system.utils.alignment_utils import attributes_in_alignment_target
 from align_system.algorithms.abstracts import ADMComponent
 from align_system.data_models.dialog import DialogElement
+
+log = logging.getLogger(__name__)
 
 
 class ICLADMComponent(ADMComponent):
@@ -65,3 +71,128 @@ class ICLADMComponent(ADMComponent):
                                                          tags=['icl']))
 
         return icl_dialog_elements
+
+
+# ICL Engines dependent on alignment target, but that could change
+# between `run` calls, don't want to reinitialize every time if we
+# don't have to
+@lru_cache
+def init_posneg_icl_engines_from_target(icl_generator_partial,
+                                        attributes,
+                                        alignment_target):
+    log.debug("Initializing positive/negative ICL generators for target")
+    kdma_values = alignment_target.kdma_values
+    if len(kdma_values) != 1:
+        raise RuntimeError("This ADM assumes a single KDMA target, aborting!")
+    kdma_value = kdma_values[0]
+    if isinstance(kdma_value, KDMAValue):
+        kdma_value = kdma_value.to_dict()
+
+    kdma = kdma_value['kdma']
+    value = kdma_value['value']
+    # Assumption here is that KDMA values range from 0-1
+    negative_value = 1 - value
+
+    positive_target = [{'kdma': kdma,
+                        'name': attributes[kdma].name,
+                        'value': value}]
+    positive_icl_generator = icl_generator_partial(positive_target)
+
+    negative_target = [{'kdma': kdma,
+                        'name': attributes[kdma].name,
+                        'value': negative_value}]
+
+    negative_icl_generator = icl_generator_partial(negative_target)
+
+    return positive_icl_generator, negative_icl_generator
+
+
+class PromptBasedICLADMComponent(ADMComponent):
+    def __init__(self,
+                 icl_generator_partial,
+                 scenario_description_template,
+                 prompt_template,
+                 attributes={}):
+        self.icl_generator_partial = icl_generator_partial
+        self.scenario_description_template = scenario_description_template
+        self.attributes = attributes
+        self.prompt_template = prompt_template
+
+    def run_returns(self):
+        return 'positive_icl_dialog_elements', 'negative_icl_dialog_elements'
+
+    def run(self,
+            scenario_state,
+            choices,
+            actions,
+            alignment_target=None):
+        if alignment_target is None:
+            raise NotImplementedError(
+                "Don't know how to generate positive/negative ICL examples "
+                "with no alignment target")
+        else:
+            target_attribute_names = attributes_in_alignment_target(alignment_target)
+
+        target_attributes = [self.attributes[n] for n in target_attribute_names]
+
+        if len(target_attributes) != 1:
+            raise ValueError("Expecting only a single attribute for alignment_target")
+        attribute = target_attributes[0]
+
+        scenario_description = call_with_coerced_args(
+                self.scenario_description_template,
+                {'scenario_state': scenario_state,
+                 'alignment_target': alignment_target,
+                 'attribute': attribute.name,
+                 'attributes_of_interest': {attribute.name,}})
+
+        prompt_to_match = call_with_coerced_args(
+                self.prompt_template,
+                {'scenario_state': scenario_state,
+                 'scenario_description': scenario_description,
+                 'choices': choices,
+                 'choice_outcomes': {c: None for c in choices},
+                 'attribute': attribute.name})
+
+        pos_icl_gen, neg_icl_gen = init_posneg_icl_engines_from_target(
+            self.icl_generator_partial,
+            self.attributes,
+            alignment_target)
+
+        pos_selected_icl_examples = pos_icl_gen.select_icl_examples(
+            sys_kdma_name=attribute.kdma,
+            scenario_description_to_match=scenario_description,
+            prompt_to_match=prompt_to_match,
+            state_comparison=scenario_state)
+        pos_icl_dialog_elements = []
+        for pos_icl_sample in pos_selected_icl_examples:
+            pos_icl_dialog_elements.append(
+                DialogElement(role='user',
+                              content=pos_icl_sample['prompt'],
+                              namespace='.icl',
+                              tags=['icl']))
+            pos_icl_dialog_elements.append(
+                DialogElement(role='assistant',
+                              content=str(pos_icl_sample['response']),
+                              namespace='.icl',
+                              tags=['icl']))
+
+        neg_selected_icl_examples = neg_icl_gen.select_icl_examples(
+            sys_kdma_name=attribute.kdma,
+            scenario_description_to_match=scenario_description,
+            prompt_to_match=prompt_to_match,
+            state_comparison=scenario_state)
+        neg_icl_dialog_elements = []
+        for neg_icl_sample in neg_selected_icl_examples:
+            neg_icl_dialog_elements.append(
+                DialogElement(role='user',
+                              content=neg_icl_sample['prompt'],
+                              namespace='.icl',
+                              tags=['icl']))
+            neg_icl_dialog_elements.append(
+                DialogElement(role='assistant',
+                              content=str(neg_icl_sample['response']),
+                              namespace='.icl',
+                              tags=['icl']))
+
+        return pos_icl_dialog_elements, neg_icl_dialog_elements
---
+++ align_system/algorithms/prompt_based_aligned_adm_component.py
@@ -0,0 +1,172 @@
+from rich.highlighter import JSONHighlighter
+from swagger_client.models import KDMAValue
+
+from align_system.utils import logging, call_with_coerced_args
+from align_system.algorithms.abstracts import ADMComponent
+from align_system.data_models.dialog import DialogElement
+from align_system.utils.voting import (
+    calculate_votes,
+    filter_votes_to_responses,
+)
+
+log = logging.getLogger(__name__)
+JSON_HIGHLIGHTER = JSONHighlighter()
+
+
+class PromptBasedAlignedADMComponent(ADMComponent):
+    def __init__(self,
+                 structured_inference_engine,
+                 scenario_description_template,
+                 prompt_template,
+                 output_schema_template,
+                 system_prompt_template,
+                 num_positive_samples=1,
+                 num_negative_samples=0,
+                 vote_calculator_fn=calculate_votes,
+                 filter_votes_to_positives=True,
+                 shuffle_choices=True):
+        self.structured_inference_engine = structured_inference_engine
+        self.scenario_description_template = scenario_description_template
+        self.prompt_template = prompt_template
+        self.output_schema_template = output_schema_template
+
+        self.system_prompt_template = system_prompt_template
+
+        self.num_positive_samples = num_positive_samples
+        self.num_negative_samples = num_negative_samples
+
+        self.vote_calculator_fn = vote_calculator_fn
+        self.filter_votes_to_positives = filter_votes_to_positives
+
+        self.shuffle_choices = shuffle_choices
+
+    def run_returns(self):
+        return ('chosen_choice', 'dialog')
+
+    def run(self,
+            scenario_state,
+            choices,
+            alignment_target):
+        kdma_values = alignment_target.kdma_values
+        if len(kdma_values) != 1:
+            raise RuntimeError("This ADM assumes a single KDMA target, aborting!")
+        kdma_value = kdma_values[0]
+        if isinstance(kdma_value, KDMAValue):
+            kdma_value = kdma_value.to_dict()
+
+        kdma = kdma_value['kdma']
+        value = kdma_value['value']
+        # Assumption here is that KDMA values range from 0-1
+        negative_value = 1 - value
+
+        scenario_description = call_with_coerced_args(
+            self.scenario_description_template,
+            {'scenario_state': scenario_state})
+
+        prompt = call_with_coerced_args(
+            self.prompt_template,
+            {'scenario_state': scenario_state,
+             'scenario_description': scenario_description,
+             'choices': choices})
+
+        positive_dialog = []
+        if self.system_prompt_template is not None:
+            positive_system_prompt = call_with_coerced_args(
+                self.system_prompt_template,
+                {'target_kdma': kdma,
+                 'target_value': value})
+
+            positive_dialog.insert(
+                0, DialogElement(role='system',
+                                 content=positive_system_prompt,
+                                 namespace='.',
+                                 tags=['regression']))
+
+        positive_dialog.append(
+            DialogElement(role='user',
+                          content=prompt,
+                          namespace='.',
+                          tags=['regression']))
+
+        positive_dialog_prompt = self.structured_inference_engine.dialog_to_prompt(
+            positive_dialog)
+
+        if self.num_negative_samples > 0:
+            negative_dialog = []
+            if self.system_prompt_template is not None:
+                negative_system_prompt = call_with_coerced_args(
+                    self.system_prompt_template,
+                    {'target_kdma': kdma,
+                     'target_value': negative_value})
+
+                negative_dialog.insert(
+                    0, DialogElement(role='system',
+                                     content=negative_system_prompt,
+                                     namespace='.',
+                                     tags=['regression']))
+
+                negative_dialog.append(
+                    DialogElement(role='user',
+                                  content=prompt,
+                                  namespace='.',
+                                  tags=['regression']))
+
+            negative_dialog_prompt = self.structured_inference_engine.dialog_to_prompt(
+                negative_dialog)
+
+        output_schema = call_with_coerced_args(
+            self.output_schema_template,
+            {'choices': choices})
+
+        log.info("[bold]*KDMA SCORE PREDICTION POSITIVE DIALOG PROMPT*[/bold]",
+                 extra={"markup": True})
+        log.info(positive_dialog_prompt)
+
+        positive_responses = self.structured_inference_engine.run_inference(
+            [positive_dialog_prompt] * self.num_positive_samples, output_schema)
+        positive_choices = [r['action_choice'] for r in positive_responses]
+
+        if self.num_negative_samples > 0:
+            negative_responses = self.structured_inference_engine.run_inference(
+                [negative_dialog_prompt] * self.num_negative_samples, output_schema)
+            negative_choices = [r['action_choice'] for r in negative_responses]
+        else:
+            negative_choices = None
+
+        votes = self.vote_calculator_fn(
+            choices, positive_choices, negative_choices)
+
+        log.explain("[bold]*VOTES*[/bold]",
+                    extra={"markup": True})
+        log.explain(votes, extra={"highlighter": JSON_HIGHLIGHTER})
+
+        if self.filter_votes_to_positives:
+            filtered_votes = filter_votes_to_responses(
+                votes, positive_choices)
+
+            if filtered_votes != votes:
+                log.explain("Filtering votes down to choices where we "
+                            "have a positive reponse")
+                log.explain(filtered_votes,
+                            extra={"highlighter": JSON_HIGHLIGHTER})
+
+            final_votes = filtered_votes
+        else:
+            final_votes = votes
+
+        # Take top choice by score (votes is a dictionary of choice: score)
+        top_choice, top_choice_score = max(final_votes.items(), key=lambda x: x[1])
+
+        # Just taking first justification from the positive responses
+        # where the top choice was selected.  A better approach might
+        # be to somehow summarized all justifications with the
+        # matching choice.  Theoretically it's possible to have no
+        # responses that match the top choice (i.e. if only using
+        # negative samples)
+        top_choice_justification = ""
+        for response in positive_responses:
+            if response['action_choice'] == top_choice:
+                top_choice_justification = response['detailed_reasoning']
+                break
+
+        return top_choice, top_choice_justification, positive_dialog
