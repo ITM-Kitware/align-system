48b85fa6ccbc6bb2272025318b8d494042ec5974
David Joy <10147749+dmjoy@users.noreply.github.com>
2025-04-18 14:05:25 -0400

    Add missing files; add variable renaming component; add adm mashup example

--- align_system/utils/generic.py
+++ align_system/utils/generic.py
@@ -1,25 +1,40 @@
 import inspect
 from inspect import Parameter
 import functools
+from collections import OrderedDict
 
 
 def call_with_coerced_args(func, dictionary, partial=False):
+    # Create a copy that we can edit and pass along if the func
+    # requests **kwargs
+    dictionary_copy = dictionary.copy()
+
     args = []
-    kwargs = {}
+    kwargs = OrderedDict()
+    pass_all = False
     for name, param in inspect.signature(func).parameters.items():
         if name in dictionary:
             if param.kind == Parameter.POSITIONAL_ONLY:
                 # Rare case, usually parameters are of kind
                 # POSITIONAL_OR_KEYWORD
-                args.append(dictionary[name])
+                args.append(dictionary_copy[name])
+                del dictionary_copy[name]
             else:
-                kwargs[name] = dictionary[name]
+                kwargs[name] = dictionary_copy[name]
+                del dictionary_copy[name]
+        elif param.kind == Parameter.VAR_KEYWORD:
+            # If func has **kwargs in it's argument signature, just
+            # pass everything
+            pass_all = True
         elif param.default != inspect._empty:
             pass  # Don't need to add to the arg/kwarg list
         else:
             if not partial:
                 raise RuntimeError(f"Don't have expected parameter "
                                    f"('{name}') in provided dictionary")
+
+    if pass_all:
+        kwargs = {**kwargs, **dictionary_copy}
 
     if partial:
         return functools.partial(func, *args, **kwargs)
