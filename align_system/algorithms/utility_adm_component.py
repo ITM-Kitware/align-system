033bf2cb5725d8a7941191b69ec1e178b93cb6c9
David Joy <10147749+dmjoy@users.noreply.github.com>
2025-05-26 15:28:56 -0400

    Add Kaleido "mashup" ADM

--- align_system/algorithms/utility_adm_component.py
+++ align_system/algorithms/utility_adm_component.py
@@ -1,6 +1,13 @@
 from collections import OrderedDict
+from functools import reduce, partial
 
+from rich.highlighter import JSONHighlighter
+
+from align_system.utils import logging
 from align_system.algorithms.abstracts import ADMComponent
+
+log = logging.getLogger(__name__)
+JSON_HIGHLIGHTER = JSONHighlighter()
 
 
 class RenameVariablesADMComponent(ADMComponent):
@@ -20,3 +27,45 @@ class RenameVariablesADMComponent(ADMComponent):
                     f"Don't have expected key ({key}) for remapping")
 
         return remapped_outputs
+
+
+def merge_dicts(a, b, conflict_func=None):
+    def _r(init, k):
+        if k in a:
+            if k in b:
+                init[k] = conflict_func(a[k], b[k]) if conflict_func else b[k]
+            else:
+                init[k] = a[k]
+        else:
+            init[k] = b[k]
+
+        return init
+
+    return reduce(_r, a.keys() | b.keys(), {})
+
+
+class MergeRegressionDictsADMComponent(ADMComponent):
+    def __init__(self, dict_names):
+        self.dict_names = dict_names
+
+    def run_returns(self):
+        return 'attribute_prediction_scores'
+
+    def run(self, **working_output):
+        def recursive_conflict_fn(a, b):
+            if isinstance(a, dict) and isinstance(b, dict):
+                return merge_dicts(a, b)
+            else:
+                return b
+
+        recursive_merge = partial(
+            merge_dicts, conflict_func=recursive_conflict_fn)
+
+        merged = reduce(
+            recursive_merge, [working_output[n] for n in self.dict_names], {})
+
+        log.debug("[bold] ** Merged Regression Dictionaries ** [/bold]",
+                  extra={"markup": True})
+        log.debug(merged, extra={"highlighter": JSON_HIGHLIGHTER})
+
+        return merged
