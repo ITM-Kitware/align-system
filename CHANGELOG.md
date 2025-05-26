e711d5d6d7739e7020387739899051722b4e25eb
Emily Veenhuis <emily.veenhuis@kitware.com>
2025-05-26 13:37:19 -0400

    Add state_hydration_domain option to ICL and input/output interface

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,14 @@
 This changelog follows the specifications detailed in: [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html), although we have not yet reached a `1.0.0` release.
 
+## Unreleased
+
+### Added
+
+* Added `state_hydration_domain` to `IncontextExampleGenerator` and `InputOutputFileInterface`. Providing a value of
+  `None` or `p1` results in Phase 1 behavior while a value of `p2triage` is meant for hydrating states from the
+  corresponding domain on the TA3 server.
+
 ## 0.5.8
 
 ### Fixed
--- align_system/utils/hydrate_state.py
+++ align_system/utils/hydrate_state.py
@@ -1,19 +1,18 @@
-from swagger_client.models import (
-    State,
-    Action,
-    Character,
-    Supplies,
-    Injury,
-    Environment,
-    DecisionEnvironment,
-    Aid,
-    SimEnvironment,
-    MetaInfo,
-)
-
-
 def hydrate_scenario_state(record):
-    """ Hydrate scenario state from record """
+    """ Hydrate scenario state from p1 record """
+    from swagger_client.models import (
+        State,
+        Action,
+        Character,
+        Supplies,
+        Injury,
+        Environment,
+        DecisionEnvironment,
+        Aid,
+        SimEnvironment,
+        MetaInfo,
+    )
+
     state = State(**record['full_state'])
     state.meta_info = MetaInfo(**state.meta_info)
     # For some reason this initialization from a dictionary
@@ -30,6 +29,24 @@ def hydrate_scenario_state(record):
             Aid(**a) for a in state.environment.decision_environment.aid]
     state.environment.sim_environment = SimEnvironment(
         **state.environment.sim_environment)
+
+    actions = [Action(**a) for a in record['choices']]
+    # TODO: Fix this on the input-output generation side, need
+    # to make sure original choices aren't being modified by
+    # ADM; for now manually clearing the justification string
+    for a in actions:
+        a.justification = None
+
+    return state, actions
+
+def p2triage_hydrate_scenario_state(record):
+    """ Hydrate scenario state from p2triage record """
+    from swagger_client.models import (
+        State,
+        Action,
+    )
+
+    state = State(**record['full_state'])
 
     actions = [Action(**a) for a in record['choices']]
     # TODO: Fix this on the input-output generation side, need
