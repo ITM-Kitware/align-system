c9c0d77e0d3916f9b92802579ce995a4b1b82350
David Joy <10147749+dmjoy@users.noreply.github.com>
2025-12-15 15:13:21 -0500

    Add caching to BertRelevance component

--- align_system/algorithms/relevance_adm_component.py
+++ align_system/algorithms/relevance_adm_component.py
@@ -1,4 +1,9 @@
+import inspect
+import re
+import copy
+
 from rich.highlighter import JSONHighlighter
+import ubelt as ub
 
 from align_system.utils import logging, call_with_coerced_args
 from align_system.algorithms.abstracts import ADMComponent
@@ -124,10 +129,13 @@ class BertRelevanceADMComponent(ADMComponent):
     def __init__(self,
                  scenario_description_template,
                  prompt_template,
-                 attributes={}):
+                 attributes={},
+                 enable_caching=False):
         self.scenario_description_template = scenario_description_template
         self.prompt_template = prompt_template
         self.attributes = attributes
+
+        self.enable_caching = enable_caching
 
     def run_returns(self):
         return ('attribute_relevance')
@@ -143,6 +151,30 @@ class BertRelevanceADMComponent(ADMComponent):
             target_attribute_names = attributes_in_alignment_target(alignment_target)
 
         target_attributes = [self.attributes[n] for n in target_attribute_names]
+
+        if self.enable_caching:
+            scenario_state_copy = copy.deepcopy(scenario_state)
+            if hasattr(scenario_state, 'elapsed_time'):
+                # Don't consider the elapsed_time of the state when caching
+                scenario_state_copy.elapsed_time = 0
+
+            depends = '\n'.join((
+                self.cache_repr(),
+                repr(scenario_state_copy),
+                repr(choices),
+                repr(icl_dialog_elements),
+                repr(target_attribute_names)))
+
+            cacher = ub.Cacher('bert_relevance_adm_component', depends, verbose=0)
+            log.debug(f'cacher.fpath={cacher.fpath}')
+
+            cached_output = cacher.tryload()
+            if cached_output is not None:
+                log.info("Cache hit for `bert_relevance_adm_component`"
+                         " returning cached output")
+                return cached_output
+            else:
+                log.info("Cache miss for `bert_relevance_adm_component` ..")
 
         attribute_relevance = {}
         if len(target_attributes) == 1:
@@ -193,4 +225,34 @@ class BertRelevanceADMComponent(ADMComponent):
                  extra={"markup": True})
         log.info("attribute_relevance: {}".format(attribute_relevance),
                  extra={"highlighter": JSON_HIGHLIGHTER})
+
+        if self.enable_caching:
+            cacher.save(attribute_relevance)
+
         return attribute_relevance
+
+    def cache_repr(self):
+        '''
+        Return a string representation of this object for caching;
+        .i.e. if the return value of this function is the same for two
+        object instances, it's assumed that `run` output will be
+        the same if given the same parameters
+        '''
+
+        def _generic_object_repr(obj):
+            init_params = inspect.signature(obj.__class__.__init__).parameters
+            obj_vars = vars(obj)
+
+            return "{}.{}({})".format(
+                obj.__class__.__module__,
+                obj.__class__.__name__,
+                ", ".join([f"{p}={obj_vars[p]}" for p in init_params
+                           if p != 'self' and p != 'args' and p != 'kwargs']))
+
+        return re.sub(r'^\s+', '',
+                      f"""
+                       {self.__class__.__module__}.{self.__class__.__name__}(
+                       scenario_description_template={_generic_object_repr(self.scenario_description_template)},
+                       prompt_template={_generic_object_repr(self.prompt_template)},
+                       attributes={self.attributes},
+                       )""", flags=re.MULTILINE).strip()
