bea6fe4e40bad317086a33e2ae663caff5e088e4
David Joy <10147749+dmjoy@users.noreply.github.com>
2026-07-10 14:46:12 -0400

    Add new ICL generator for July 2026 OW experiments

--- align_system/utils/incontext_utils.py
+++ align_system/utils/incontext_utils.py
@@ -887,3 +887,80 @@ class Phase2ComparativeRegressionIncontextExampleGenerator(IncontextExampleGener
 
         cot_reasoning = f"{max_choice} demonstates {adjective} more {target_kdma['name']} than {min_choice}."
         return cot_reasoning
+
+# TODO: Refactor IncontextExampleGenerators to take scenario
+# description and prompt templates as arguments and use
+# `call_with_coerced_args`
+class Phase2ComparativeRegressionIncontextExampleGeneratorOWConversion(Phase2ComparativeRegressionIncontextExampleGenerator):
+    def set_icl_datasets(self):
+        from swagger_client.models import ActionTypeEnum
+
+        icl_datasets = {}
+        incontext_data = self._read_icl_dataset_files()
+
+        # Add each target to icl_datasets
+        for target_kdma in self.target_kdmas:
+            sys_kdma_name = target_kdma['kdma']
+            icl_datasets[sys_kdma_name] = []
+            kdma_incontext_data = incontext_data[sys_kdma_name]
+
+            # Add each examples to icl_datasets
+            for example in kdma_incontext_data:
+                character_unstructured_by_id = {c.id: c.unstructured for c in example['state'].characters}
+
+                # Get example response
+                icl_response = {}
+                included_choices = []
+                for action, choice, kdma_value in zip(example['actions'], example['choices'], example["kdma_values"]):
+                    # HACK: Reformat choice string for OW
+                    # TODO: Bring more in line with OWFormatChoicesADMComponent (align_system/algorithms/open_world_components.py)
+                    if action.action_type in {ActionTypeEnum.END_SCENE, ActionTypeEnum.SEARCH}:
+                        choice = action.unstructured
+                    else:
+                        c_id = action.character_id
+                        # Not doing action expansion here as is done
+                        # in the OWFormatChoiceADMComponent, since
+                        # it's not clear whether the ground truth
+                        # values would just be duplicated across the
+                        # expansion or??
+                        assert c_id is not None
+
+                        choice = f"{c_id}: {character_unstructured_by_id[c_id]}"
+
+                    # Only include choice if there is a ground truth KDMA value available
+                    if kdma_value is None:
+                        continue
+                    # Groundtruth KDMA values are 0-1, but ADM may predict on a different scale
+                    scaled_kdma_value = int(kdma_value * target_kdma["factor"])
+                    icl_response[choice] = {}
+                    icl_response[choice]['score'] = scaled_kdma_value
+                    included_choices.append(choice)
+                icl_response_with_reasoning={}
+                icl_response_with_reasoning['reasoning'] = self.get_chain_of_thought_reasoning(target_kdma, icl_response)
+                icl_response_with_reasoning.update(icl_response) # reasoning first
+                # Check if response is valid against json schema
+                correct_schema = json.loads(comparative_regression_json_schema(included_choices, target_kdma["factor"]))
+                validate(instance=icl_response_with_reasoning, schema=correct_schema)
+
+                # Get example prompt
+                icl_scenario_description = phase2_scenario_state_description(example['state'])
+                # Only include choices in the prompt if they are in the response
+                included_icl_choices_with_outcomes = {}
+                for choice in included_choices:
+                    # TODO: Include outcome prediction for ICL examples?
+                    included_icl_choices_with_outcomes[choice] = {'predicted_outcome':None}
+
+                icl_prompt = comparative_regression_prompt(icl_scenario_description,
+                                                           included_icl_choices_with_outcomes,
+                                                           target_kdma['name'])
+
+                # Add example
+                icl_datasets[sys_kdma_name].append({
+                    "state": example["state"],
+                    "scenario_description": icl_scenario_description,
+                    "prompt": icl_prompt,
+                    "response": icl_response_with_reasoning,
+                    "actions": example['actions']
+                    })
+
+        self.icl_datasets = icl_datasets
